d102088e035aad35a3c0f1b72946280bdf4d269e
marceloaraujo <marceloaraujo14@hotmail.com>
2019-05-05 13:18:05 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
 
 import 'package:flutter/material.dart';
 import 'package:herplin_beta/menu.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   // This widget i/**/s the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       title: 'Flutter Code Sample for material.Scaffold',
       theme: ThemeData(
@@ -28,43 +19,32 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  
-    MyStatefulWidget({Key key}) : super(key: key);
+
+  MyStatefulWidget({Key key}) : super(key: key);
 
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
-  int _count = 0;
-
+  
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Menu().getMenu(this.context),    //constroi menu
+      drawer: Menu().getMenu(this.context),
+      //constroi menu
       appBar: AppBar(
-        title: Text('HERPLIN beta'),
+        title: Text('AppName'),
       ),
-      
       body: Center(
-
-        child: Text('Você have pressed the button $_count times.'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
+        child: new Column(
+          children: [
+            Padding(padding: EdgeInsets.all(10.0)),
+            Expanded(child: Image.network('https://organicsnewsbrasil.com.br/wp-content/uploads/2015/10/Carrefour-c-logo.png',scale: 10,)),
+            Expanded(child: Image.network('https://organicsnewsbrasil.com.br/wp-content/uploads/2015/10/Carrefour-c-logo.png',scale: 10,)),
+            Expanded(child: Image.network('https://organicsnewsbrasil.com.br/wp-content/uploads/2015/10/Carrefour-c-logo.png',scale: 10,)),
+          ]        
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-              _count++;
-            }),
-        tooltip: 'Increment Counter',
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
-
-    
   }
 }
--- lib/servicos.dart
+++ lib/servicos.dart
@@ -5,58 +5,77 @@ class Servicos extends StatefulWidget {
   _State createState() => _State();
 }
 
+//Detalhes do anunciante
 
- detalhes(){
-
-return Scaffold(
-  appBar: AppBar(
-      title: Text("Barbearia do Zé"),
+Widget textSection = Container(
+ padding: EdgeInsets.all(15.0),
+  child: Text(
+    ' A barbearia do Zé está no bairro há maisde 20 anos'
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
   ),
-    body: Container(
-
-      padding: EdgeInsets.all(15.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Image.network('http://cdn.abcdoabc.com.br/Barbearia-Burning-House-1_f47e9c2a.jpg')
-        ,
-      ),
-    ),
-  
 );
 
+Widget fotoSection = Container(
+  
+child: ClipRRect(
 
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              child: Image.network(
+                  'http://cdn.abcdoabc.com.br/Barbearia-Burning-House-1_f47e9c2a.jpg'),
+            ),
+          ),
+
+);
+
+detalhes() {
+
+  return
+    Scaffold(
+      
+      appBar: AppBar(
+        title: Text("Barbearia do Zé"),
+      ),
+      body:  Column(
+
+        
+        children: <Widget>[
+          fotoSection,
+        textSection
+        ],
+      ));
 }
 
 class _State extends State<Servicos> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       backgroundColor: Colors.white,
-
       appBar: AppBar(
         title: Text('Serviços'),
       ),
       body: ListView(
-        children:  <Widget>[
-
+        children: <Widget>[
           ListTile(
-
             title: Text("Barbearia"),
             subtitle: Text("Barberaria do Zé"),
-            trailing: Text("tel.: 96477-1233", style:TextStyle(fontSize: 15)),
+            trailing: Text("tel.: 96477-1233", style: TextStyle(fontSize: 15)),
             leading: CircleAvatar(
-                backgroundImage: NetworkImage("https://brandmark.io/logo-rank/random/beats.png")),
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>detalhes()));
+                backgroundImage: NetworkImage(
+                    "https://brandmark.io/logo-rank/random/beats.png")),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => detalhes()));
             },
           ),
-
         ],
-
       ),
-
     );
   }
 }
